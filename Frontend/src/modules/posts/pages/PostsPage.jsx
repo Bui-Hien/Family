9453342cf22110c06eaf58416684e7f691c26a14
hienbx <hienbx@lifetex.vn>
2026-07-12 21:16:27 +0700

    view detail

--- Frontend/src/modules/posts/pages/PostsPage.jsx
+++ Frontend/src/modules/posts/pages/PostsPage.jsx
@@ -1,21 +1,13 @@
 import React, { useEffect } from 'react';
-import {
-  Box,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Typography,
-  Button
-} from '@mui/material';
+import { Box } from '@mui/material';
 import { usePostStore } from '@/modules/posts/store/usePostStore';
 import PostToolbar from './PostToolbar';
 import PostList from './PostList';
 import PostForm from './PostForm';
 import PostFilter from './PostFilter';
+import PostViewDialog from './PostViewDialog';
 import CommonLoading from '@/common/components/display/CommonLoading';
 import CommonConfirmDialog from '@/common/components/popup/CommonConfirmDialog';
-import { PostCategory } from '@/common/constants';
 
 const PostsPage = () => {
   const {
@@ -26,7 +18,6 @@ const PostsPage = () => {
     openConfirmDeletePopup,
     openCreateEditPopup,
     openViewPopup,
-    selectedRow,
     handleClose,
     handleConfirmDelete
   } = usePostStore();
@@ -56,27 +47,7 @@ const PostsPage = () => {
       {openCreateEditPopup && <PostForm />}
 
       {/* Viewing Post Detail Dialog */}
-      <Dialog open={openViewPopup} onClose={handleClose} maxWidth="md" fullWidth>
-        {selectedRow && (
-          <>
-            <DialogTitle sx={{ fontWeight: 700, pb: 1 }}>
-              {selectedRow.title}
-            </DialogTitle>
-            <DialogContent dividers>
-              <Typography variant="caption" color="textSecondary" sx={{ display: 'block', mb: 2 }}>
-                Ngày đăng: {selectedRow.createdDate ? new Date(selectedRow.createdDate).toLocaleString('vi-VN') : ''} • Danh mục: {selectedRow.category === PostCategory.TIN_TUC ? 'Tin tức chung' : 'Hoạt động dòng họ'}
-              </Typography>
-              <Typography variant="subtitle1" sx={{ fontWeight: 600, color: 'text.secondary', mb: 2, fontStyle: 'italic' }}>
-                {selectedRow.summary}
-              </Typography>
-              <Typography variant="body1" sx={{ whiteSpace: 'pre-line', lineHeight: 1.6 }} dangerouslySetInnerHTML={{ __html: selectedRow.content }} />
-            </DialogContent>
-            <DialogActions sx={{ px: 3, py: 1.5 }}>
-              <Button onClick={handleClose} color="primary" variant="contained">Đóng</Button>
-            </DialogActions>
-          </>
-        )}
-      </Dialog>
+      {openViewPopup && <PostViewDialog />}
 
       {/* Confirm Delete Dialog */}
       <CommonConfirmDialog
@@ -91,3 +62,4 @@ const PostsPage = () => {
 };
 
 export default PostsPage;
+
